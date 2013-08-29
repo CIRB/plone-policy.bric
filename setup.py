6a88a7553fdddf15930dd3ea4056f512335a292e
Stéphane Moussiaux <smoussiaux@cirb.irisnet.be>
2013-08-29 07:33:29 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,70 @@
+from setuptools import setup, find_packages
+
+version = '1.0'
+
+setup(name='policy.bric',
+      version=version,
+      description="policy of BRIC site",
+      long_description=open("README.rst").read() + "\n" +
+                       open("CHANGES.txt").read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='',
+      author_email='',
+      url='https://github.com/collective/',
+      license='gpl',
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      namespace_packages=['policy'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'Plone',
+          'Products.LinguaPlone',
+          'cirb.zopemonitoring',
+          'collective.stomach',
+          # -*- Extra requirements: -*-
+          'plonetheme.bric',
+          'collective.ckeditor',
+          'collective.plonefinder',
+          'collective.quickupload',
+          'quintagroup.analytics',
+          'fourdigits.portlet.twitter',
+          'collective.anysurfer',
+          'Products.PloneHelpCenter',
+          'collective.contentstats',
+          'collective.recaptcha',
+          'Products.Collage',
+          'webcouturier.dropdownmenu',
+          'collective.collage.portlets',
+          'cirb.zopemonitoring',
+          'cirb.blog',
+          #extra feature for blog
+          'archetypes.linguakeywordwidget',
+          'collective.categories',
+          'collective.diggdigg',
+          'collective.portlet.oembed',
+          'collective.portlet.twittermultistream',
+          'collective.portlet.socialnetworks',
+          'collective.configviews',
+          'collective.portlet.categories',
+          'collective.portlet.keywords',
+          #/blog
+          'plone.api',  # to make upgrade steps easier to write
+          #'plone.app.multilingual',
+          #'plone.multilingualbehavior',
+          'collective.languagemovefolders',
+          'collective.checktranslated',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
